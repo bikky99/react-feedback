9cf96709c347560b40c5e5a2df0abc8614332466
Bikas Dahal <115933874+bikky99@users.noreply.github.com>
2022-10-19 06:34:30 +0545

    button created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import feedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
     <>
         <Header text='Feedback UI' /> 
         <div className='container'>
+          <FeedbackForm />
             <FeedbackStats feedback = {feedback} />
             <FeedbackList feedback = {feedback} handleDelete = {deleteFeedback} />
         </div>
